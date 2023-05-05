e91c67c767c03c6656bf475810b3a26ef21ca937
marianopettinati <marianopettinati@gmail.com>
2023-05-05 19:40:36 -0300

    avances sobre products view y productDetails modal

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal">
+      <slot
+        >Default content - aparezco solo si no me pasan contenido para el
+        slot</slot
+      >
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: [""],
+  methods: {
+    closeModal() {
+      console.log("backdrop");
+      this.$emit("close");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  width: 600px;
+  height: 600px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+.backdrop {
+  top: 0;
+  left: 0;
+  position: fixed;
+  background: rgba(34, 40, 49, 0.6);
+  width: 100%;
+  height: 100%;
+}
+
+.modal h1 {
+  color: darkolivegreen;
+}
+
+.modal .actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+}
+
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+
+.warning {
+  background-color: red;
+}
+
+.success {
+  background-color: green;
+}
+</style>
--- src/views/ProductsSample.vue
+++ src/views/ProductsSample.vue
@@ -1,3 +1,5 @@
+COSAS A VER: - Responsiveness
+
 <template>
   <div>
     <Header />
@@ -6,62 +8,86 @@
   <div class="products">
     <div
       class="product"
-      @mouseenter="mouseEnter"
-      @mouseleave="mouseLeave"
-      :class="{ productActive: productIsActive }"
-      v-for="product in products"
+      v-for="(product, index) in products"
+      :key="product.id"
+      @click="openProductDetails(index)"
     >
-      <!-- <img :src="fotoPortada" alt="imagen" /> -->
-      <img src="{{product.fotoPortada}}" alt="{{product.name}}" />
+      <img class="bottom" :src="product.fotoPortadaAlt" :alt="product.name" />
+      <img class="top" :src="product.fotoPortada" :alt="product.name" />
+      <!-- <img
+        :src="product.mouseOver ? product.fotoPortadaAlt : product.fotoPortada"
+        :alt="product.name"
+      /> -->
       <div class="articulo">
         <h3>{{ product.nombre }}</h3>
         <p>{{ product.descripcion }}</p>
       </div>
     </div>
+    <ProductDetail v-if="showProductDetails" @close="closeProductDetails">
+      <template v-slot>
+        <h1>{{ selectedProduct.nombre }}</h1>
+        <p>{{ selectedProduct.descripcion }}</p>
+        <img :src="selectedProduct.fotoPortada" :alt="selectedProduct.nombre" />
+      </template>
+    </ProductDetail>
   </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
+import ProductDetail from "../components/ProductDetails.vue";
 
 export default {
-  components: { Header },
+  components: { Header, ProductDetail },
   data() {
     return {
-      productIsActive: false,
-      fotoPortada: "",
+      showProductDetails: false,
+      selectedProduct: "",
       products: [
         {
-          nombre: "Mesa Coqui",
-          descripcion: "Mesa ratona en guayubira y cemento blanco",
-          fotoPortada: require("../assets/MesaRatoaGuayubiraFrenteNoBG.png"),
-          fotoPortadaAlt: require("../assets/pebblely.png"),
+          id: 0,
+          nombre: "Perchero Ary",
+          descripcion: "Perchero con estante en paraíso laqueado",
+          fotoPortada: require("../assets/percheroAryFrente.jpeg"),
+          fotoPortadaAlt: require("../assets/percheroAryIso.jpeg"),
+          mouseOver: false,
         },
         {
+          id: 1,
           nombre: "Velador Tangible",
           descripcion: "Velador de amurar en paraíso y cemento blanco",
-          fotoPortada: require("../assets/MesaRatoaGuayubiraFrenteNoBG.png"),
-          fotoPortadaAlt: require("../assets/pebblely.png"),
+          fotoPortada: require("../assets/velador_tangible.png"),
+          fotoPortadaAlt: require("../assets/velador_tangible_perspectiva.png"),
+          mouseOver: false,
         },
         {
+          id: 2,
           nombre: "Biblio Catalina",
           descripcion:
             "Biblioteca en tres módulos en paraíso y melamina blanca, con lugar de guardado",
-          fotoPortada: require("../assets/MesaRatoaGuayubiraFrenteNoBG.png"),
-          fotoPortadaAlt: require("../assets/pebblely.png"),
+          fotoPortada: require("../assets/bibliotecaCatalinaSobreFondoDeHormigon.jpg"),
+          fotoPortadaAlt: require("../assets/biblioCataFrente.png"),
+          mouseOver: false,
+        },
+        {
+          id: 3,
+          nombre: "Mesa Coqui",
+          descripcion: "Mesa ratona en Guayubira y cemento blanco",
+          fotoPortada: require("../assets/mesaRatonaGuayubiraFrenteNoBG.png"),
+          fotoPortadaAlt: require("../assets/mesaRatonaGuayubiraIso.png"),
+          mouseOver: false,
         },
       ],
     };
   },
-  mounted() {
-    this.fotoPortada = this.products[0].fotoPortada;
-  },
   methods: {
-    mouseEnter() {
-      this.fotoPortada = this.products[0].fotoPortadaAlt;
+    openProductDetails(index) {
+      console.log("click", index);
+      this.showProductDetails = true;
+      this.selectedProduct = this.products[index];
     },
-    mouseLeave() {
-      this.fotoPortada = this.products[0].fotoPortada;
+    closeProductDetails() {
+      this.showProductDetails = false;
     },
   },
 };
@@ -74,20 +100,35 @@ export default {
   padding: 0 5%;
 }
 
-.productActive {
-  background: #dcd7c9;
-}
-
 .product {
+  position: relative;
+  /* margin: 0 auto; */
+
   display: flex;
   flex-direction: column;
-  height: 500px;
-  width: 350px;
+  height: 400px;
+  width: 300px;
   padding: 15px;
   border-radius: 10px;
   text-align: center;
   box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
 }
+
+.product img {
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  /* -webkit-transition: opacity 0.5s ease-in-out;
+  -moz-transition: opacity 0.5s ease-in-out;
+  -o-transition: opacity 0.5s ease-in-out; */
+  transition: opacity 0.5s ease-in-out;
+}
+
+.top:hover {
+  opacity: 0;
+}
+
 .articulo {
   text-align: left;
   margin-top: auto;
